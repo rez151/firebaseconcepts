78475abc2d2708b3f63c7c4dfcd18975ecb42130
Christian Reser <christian.reser@haufe-lexware.net>
2023-10-10 23:54:43 +0200

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count iss {count}
         </button>
           <Button>asdf</Button>
         <p>
