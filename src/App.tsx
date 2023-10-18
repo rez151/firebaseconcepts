7a3669a627fc1b30e27883a567443403cb430449
Christian Reser <christian.reser@haufe-lexware.net>
2023-10-18 23:37:57 +0200

    login request

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import {Navigate, Route, Routes} from 'react-router-dom';
 import Dashboard from "./components/hub/Dashboard";
 import {useState} from "react";
 
+
 export default function App() {
 
-  const [auth] = useState<boolean>(false);
+  const [auths] = useState<boolean>(false);
 
   return (<Routes>
-      <Route path="/" element={<Navigate to={auth ? "/home" : "/login"}/>}/>
+      <Route path="/" element={<Navigate to={auths ? "/home" : "/login"}/>}/>
       <Route path="/login" element={<SignIn/>}/>
       <Route path="/home" element={<Dashboard/>}/>
     </Routes>
+
   )
 }
 
